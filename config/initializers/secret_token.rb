3a5ff04b6410695e6ae98d15cde806fb40bb1fcb
Barry Mitchelson <barry.mitchelson@gmail.com>
2012-01-18 17:18:47 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linksapp::Application.config.secret_token = '96ae6bebd5a6e1e151422fa8bb781ee2e77ded6b0d38b5712716f7b64dd9642b8b3de61e87a57056338d10a140bb7f860a263b34a1cb86cd446e9e2affacbdaf'
