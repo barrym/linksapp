69f4a5ebebdb108858a3ffcbd75212abf2d93954
Barry Mitchelson <barry.mitchelson@gmail.com>
2012-01-25 15:38:36 +0000

    Check for ENV before loading omniauth.yaml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    @twitter = YAML.load(File.read(Rails.root.join("config/omniauth.yaml")))['twitter']
+    if ENV['TWITTER_KEY'] && ENV['TWITTER_SECRET']
+      @twitter = {
+        'key'    => ENV['TWITTER_KEY'],
+        'secret' => ENV['TWITTER_SECRET']
+      }
+    else
+      @twitter = YAML.load(File.read(Rails.root.join("config/omniauth.yaml")))['twitter']
+    end
     provider :twitter, @twitter['key'], @twitter['secret']
 end
