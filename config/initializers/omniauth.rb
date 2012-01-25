5c24b02d61fa8095afd36645a8f62f33a0f4f283
Barry Mitchelson <barry.mitchelson@gmail.com>
2012-01-25 16:12:47 +0000

    Changed omniauth to ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    if ENV['TWITTER_KEY'] && ENV['TWITTER_SECRET']
-      @twitter = {
-        'key'    => ENV['TWITTER_KEY'],
-        'secret' => ENV['TWITTER_SECRET']
-      }
-    else
-      @twitter = YAML.load(File.read(Rails.root.join("config/omniauth.yaml")))['twitter']
-    end
-    provider :twitter, @twitter['key'], @twitter['secret']
+    provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
