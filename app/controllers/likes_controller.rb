964790e43f0fc25da55650020c1eabc7fa92d61b
Barry Mitchelson <barry.mitchelson@gmail.com>
2012-02-12 22:25:12 +0000

    Don't change updated at when unliking.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,8 +8,8 @@ class LikesController < ApplicationController
       @link.likers.delete(current_user)
     else
       @link.likers << current_user
+      @link.update_attributes :updated_at => Time.now
     end
-    @link.update_attributes :updated_at => Time.now
     respond_with(@link.reload)
   end
 
