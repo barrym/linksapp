fa284105da86f7565302d32e5f1c8d95fb88de33
Barry Mitchelson <barry.mitchelson@gmail.com>
2012-02-12 17:40:08 +0000

    Replaced bookmarklet link with JS srced version.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,14 +5,17 @@ class LinksController < ApplicationController
   respond_to :html, :json, :js
 
   def add
-    link = Link.find_by_url(params[:url])
-    if link
-      flash[:notice] = "This link has already been shared, a like has been added for you to the original post. THIS DOES NOT WORK."
-      redirect_to link_path(link)
+    if !params[:url]
     else
-      link = current_user.links.build(:url => params[:url], :title => params[:title], :type => 'website')
-      link.save!
-      redirect_to link_path(link)
+      link = Link.find_by_url(params[:url])
+      if link
+        flash[:notice] = "This link has already been shared, a like has been added for you to the original post. THIS DOES NOT WORK."
+        redirect_to link_path(link)
+      else
+        link = current_user.links.build(:url => params[:url], :title => params[:title], :type => 'website')
+        link.save!
+        redirect_to link_path(link)
+      end
     end
   end
 
@@ -22,7 +25,6 @@ class LinksController < ApplicationController
   end
 
   def index
-    # @links = Link.order('updated_at desc').includes(:source, :user)
     @links = Link.by_day
     respond_with @links
   end
---
+++ app/views/links/add.js.coffee
@@ -0,0 +1,1 @@
+window.location.replace("<%= links_add_url %>?url=" + encodeURIComponent(document.location.href) + "&title=" + encodeURIComponent(document.title))
